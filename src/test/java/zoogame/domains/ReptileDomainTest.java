a60ce6648ac216e34f05679aaf5eeb872914aa3c
Illia Ruban <ilyruban@gmail.com>
2025-10-24 21:39:13 +0200

    Created test for Reptile Domain(passed)

---
+++ src/test/java/zoogame/domains/ReptileDomainTest.java
@@ -0,0 +1,28 @@
+package zoogame.domains;
+
+import org.junit.jupiter.api.Test;
+import zoogame.animals.Reptile;
+import zoogame.animals.SizeClass;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReptileDomainTest {
+
+    Reptile crocodile = new Reptile("Crocodile", 3500.0, SizeClass.BIG,
+            10, 2, 300);
+    ReptileDomain domain = new ReptileDomain();
+
+    @Test
+    void rumbleStarted()
+    {
+        domain.addAnimal(crocodile);
+        for (int i = 0; i < 2; i++) {
+            Reptile copy = new Reptile(crocodile);
+            domain.addAnimal(copy);
+        }
+        domain.setRumbleTime(true);
+        domain.startRumble();
+        assertEquals(2, domain.getCurrentAmountOfAnimals());
+    }
+
+}
